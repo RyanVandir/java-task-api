2eff7643bd8037b61992eb876e402bc23e60ef94
RyanVandir <r.vandir@outlook.com>
2026-08-06 14:40:49 -0300

    feat(Task): implement created and findAll service

---
+++ src/main/java/com/ryan/task_api/service/TaskService.java
@@ -0,0 +1,23 @@
+package com.ryan.task_api.service;
+
+import com.ryan.task_api.model.Task;
+import com.ryan.task_api.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public Task create(Task task){
+        return taskRepository.save(task);
+    }
+
+    public List<Task> findAll(){
+        return taskRepository.findAll();
+    }
+}
